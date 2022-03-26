7ffe1768b8ff390f9a3e5b98f59567f51f43430d
Gustavo Porto <gustavoporto@ya.ru>
2022-03-26 14:25:23 +0000

    chore: update config.def.h

--- config.def.h
+++ config.def.h
@@ -127,7 +127,7 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
-unsigned int defaultcs = 256;
+static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
 unsigned int defaultitalic = 7;
 unsigned int defaultunderline = 7;
